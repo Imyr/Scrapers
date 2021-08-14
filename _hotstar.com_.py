3159f291bd8994d648d5c5648905ac2a5027446f
Shashwat Verma <68842852+Imyr@users.noreply.github.com>
2021-08-14 14:32:29 +0530

    Update _hotstar.com_.py

--- _hotstar.com_.py
+++ _hotstar.com_.py
@@ -96,34 +96,42 @@ m3u8_name = "master.m3u8"
 mpd_url = None
 m3u8_url = None
 
-
 if AUTO_MODE:
     mpd_name = "master-tv.mpd"
     m3u8_name = "master.m3u8"
+    tags = "encryption:plain"
 else:
     mpd_name = input("MPD Name: ")
+    m3u8_name = input("M3U8 Name: ")
+    tags = input("Tag Combination: ")
 
 for i in api_resp["data"]["playBackSets"]:
-    if mpd_name in i["playbackUrl"]:
+    if mpd_name in i["playbackUrl"] and tags in i["tagsCombination"]:
         mpd_url = i["playbackUrl"]
         break
-    if m3u8_name in i["playbackUrl"]:
+    if m3u8_name in i["playbackUrl"] and tags in i["tagsCombination"]:
         m3u8_url = i["playbackUrl"]
         break
 
 if not mpd_url:
     playlist_base = m3u8_url.split(m3u8_name)[0]
     m3u8 = hotstar.get(m3u8_url, headers=MPD_HEADERS).text
-    print(bs(m3u8, "html.parser").prettify())
+    subindex_id = False
     stream_ids = []
+    subindex_ids = re.findall(r'URI="(.*)"', m3u8)
+
     for i in m3u8.split('\n'):
         if ".m3u8" in i:
             stream_ids.append(i.replace(playlist_base, ""))
 
     if AUTO_MODE:
         stream_id = stream_ids[-1]
+        if subindex_ids:
+            subindex_id = subindex_ids[-1]
     else:
-        stream_id = input("Index Name: ")
+        stream_id = input("Stream ID: ")
+        if subindex_ids:
+            subindex_id = input("Subtitle Index ID: ")
 
     index_url = playlist_base + stream_id
     index = hotstar.get(index_url, headers=MPD_HEADERS).text
@@ -134,18 +142,53 @@ if not mpd_url:
         if ".ts" in i:
             segment_ids.append(i.replace(playlist_base, ""))
 
-    with open(f"{ID}.ts", "wb") as strm:
+    Subtitle = False
+    if subindex_id:
+        subtitle_index = hotstar.get(playlist_base + subindex_id, headers=MPD_HEADERS).text
+
+        subtitle_ids = []
+        for i in subtitle_index.split("\n"):
+            if not "#EXT" in i:
+                subtitle_ids.append(i)
+        print(subtitle_ids)
+
+        if AUTO_MODE:
+            subtitle_id = subtitle_ids[-1]
+        else:
+            subindex_id = input("Subtitle ID: ")
+
+        subtitle_url = playlist_base + subtitle_id
+        with open(f'{ID}-sub.vtt', 'wb') as sub:
+            SUB = hotstar.get(subtitle_url, headers=MPD_HEADERS)
+            if SUB.status_code == 200:
+                print(f"OK: {ID}-{subtitle_id}")
+                sub.write(SUB.content)
+                Subtitle = True
+            
+    with open(f"{ID}-strm.ts", "wb") as strm:
         for i in segment_ids:
             TS = hotstar.get(playlist_base + i, headers=MPD_HEADERS)
             if TS.status_code == 200:
                 print(f"OK: {ID}-{i}")
                 strm.write(TS.content)
-                
-    subprocess.run(["ffmpeg",
-                    "-i", f"{ID}.ts", 
-                    "-map", "0",
-                    "-c", "copy",
-                    f'{ID}.mp4'])
+
+    if Subtitle:
+        ffmpeg_command = ["ffmpeg",
+                        "-i", f"{ID}-strm.ts", 
+                        "-i", f"{ID}-sub.vtt",
+                        "-map", "0",
+                        "-map", "1",
+                        "-c", "copy",
+                        "-c:s", "srt",
+                        f'{ID}.mp4']
+    else:
+        ffmpeg_command = ["ffmpeg",
+                        "-i", f"{ID}-strm.ts", 
+                        "-map", "0",
+                        "-c", "copy",
+                        f'{ID}.mkv']
+
+    subprocess.run(ffmpeg_command)
 
 else:
     playlist_base = mpd_url.split(mpd_name)[0]
@@ -153,31 +196,47 @@ else:
 
     print(bs(mpd, "html.parser").prettify())
 
+    subtitle_id = False
+
     video_ids = []
     audio_ids = []
+    subtitle_ids = []
 
     for i in bs(mpd, "lxml").find_all("representation"):
         if "video" in i.get("id"):
             video_ids.append(i.get("id"))
         elif "audio" in i.get("id"):
             audio_ids.append(i.get("id"))
+        elif "subtitle" in i.get("id"):
+            subtitle_ids.append(i.get("id"))
 
     print(video_ids)
     print(audio_ids)
+    print(subtitle_ids)
 
     if AUTO_MODE:
         video_id = video_ids[-1]
         audio_id = audio_ids[-1]
+        if subtitle_ids:
+            subtitle_id = subtitle_ids[-1]
     else:
         video_id = input("Video ID: ")
         audio_id = input("Audio ID: ")  
+        subtitle_id = input("Subtitle ID: ")
         
     video_base_url = f"{mpd_url.split(mpd_name)[0]}{video_id}/"
     audio_base_url = f"{mpd_url.split(mpd_name)[0]}{audio_id}/"
-
-
-
+    Subtitle = False
     #DOWNLOAD
+    if subtitle_id:
+        subtitle_url = f"{mpd_url.split(mpd_name)[0]}{subtitle_id}.vtt"
+        with open(f'{ID}-sub.vtt', 'wb') as sub:
+            SUB = hotstar.get(subtitle_url, headers=MPD_HEADERS)
+            if SUB.status_code == 200:
+                print(f"OK: {ID}-S")
+                sub.write(SUB.content)
+                Subtitle = True
+            
     with open(f"{ID}-vid.m4s", "wb") as vid:
         c = 1
         INIT = hotstar.get(video_base_url + "init.mp4", headers=MPD_HEADERS)
@@ -206,10 +265,24 @@ else:
             else:
                 break
 
-    subprocess.run(["ffmpeg",
-                    "-i", f"{ID}-vid.m4s",
-                    "-i", f"{ID}-aud.m4s", 
-                    "-map", "0",
-                    "-map", "1", 
-                    "-c", "copy",
-                    f'{ID}.mp4'])
+    if Subtitle:
+        ffmpeg_command = ["ffmpeg",
+                        "-i", f"{ID}-vid.m4s",
+                        "-i", f"{ID}-aud.m4s",
+                        "-i", f"{ID}-sub.vtt", 
+                        "-map", "0",
+                        "-map", "1",
+                        "-map", "2", 
+                        "-c", "copy",
+                        "-c:s", "srt",
+                        f'{ID}.mp4']
+    else:
+        ffmpeg_command = ["ffmpeg",
+                        "-i", f"{ID}-vid.m4s",
+                        "-i", f"{ID}-aud.m4s", 
+                        "-map", "0",
+                        "-map", "1", 
+                        "-c", "copy",
+                        f'{ID}.mp4']
+
+    subprocess.run(ffmpeg_command)
